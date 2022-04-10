9648d077620081e0eebcf6315d2dc484bf48d57c
TheVosges <79966556+TheVosges@users.noreply.github.com>
2022-04-10 22:56:01 +0200

    Working login database

--- app/src/main/java/com/example/gamezone/LoginActivity.java
+++ app/src/main/java/com/example/gamezone/LoginActivity.java
@@ -2,11 +2,19 @@ package com.example.gamezone;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -24,12 +32,33 @@ public class LoginActivity extends AppCompatActivity {
 }
 
     public void onButLoginClicked (View view){
+        //Tworzenie ciągu zapytania;
+        String url = "http://10.0.2.2:5000/users/" + username.getText().toString() + "&" + password.getText().toString();
+//        String url = "http://127.0.0.1:5000/users/" + username.getText().toString() + "&" + password.getText().toString();
+        Log.i("mes", url);
+        System.out.println(url);
 
-        if (username.getText().toString().equals("admin") && password.getText().toString().equals("123")){
+        //Wysłanie zapytanie i oczekiwanie na odpowiedź w OnResponse i OnErrorResponse
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, this::onResponse, this::onErrorResponse);
+        queue.add(stringRequest);
+
+
+        
+    }
+
+    private void onResponse(String s) {
+        String integer = s.toString();
+        System.out.println(integer);
+        integer = integer.replaceAll("\"","" );
+        System.out.println("tu");
+        if (Integer.parseInt(integer.toString().trim()) != 0){
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         } else Toast.makeText(this, "Incorrect login or password", Toast.LENGTH_LONG).show();
-        
+    }
+    private void onErrorResponse(VolleyError error) {
+        Toast.makeText(this, "Incorrect login or password", Toast.LENGTH_LONG).show();
     }
 
     public void finishLoginActivity (View view){ finish(); }
